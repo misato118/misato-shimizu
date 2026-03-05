ba9a5a26be298240c665be2fb634e2896425cec0
misato118 <mtsz.29910@gmail.com>
2026-03-05 14:44:43 -0500

    Added base structure for the intro section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ScrollReveal from "@/components/ScrollReveal"
 
 export default function Home() {
   return (
-    <div className="h-screen overflow-y-scroll snap-y snap-mandatory">
+    <div className="h-screen overflow-y-scroll snap-y snap-mandatory bg-base-300">
       <div className="h-full flex flex-col snap-center">
         <header className="bg-neutral h-auto">
           <div className="flex justify-end-safe p-6 lg:px-8 gap-x-4">
@@ -12,7 +12,10 @@ export default function Home() {
         </header>
         <div className="grow grid place-content-center">
           <ScrollReveal>
-            <div>A</div>
+            <div className="max-w-md mx-auto text-center">
+              <h1 className="text-5xl font-bold mb-4 text-primary">Misato Shimizu</h1>
+              <p className="leading-relaxed text-base-content/80">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
+            </div>
           </ScrollReveal>
         </div>
       </div>
