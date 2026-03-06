977a0eb290185bfcf92780b2457f6e0e12c414a7
misato118 <mtsz.29910@gmail.com>
2026-03-06 18:09:46 -0500

    Added a download resume function

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -12,7 +12,9 @@ const NavBar = () => {
     <header className="bg-neutral h-auto">
       <div className="flex justify-end-safe p-6 lg:px-8 gap-x-4">
         <button onClick={() => scrollToPlayground("playground")} className="btn btn-secondary btn-circle w-fit p-3 text-neutral-content">Check Playground</button>
-        <button className="btn btn-primary btn-circle w-fit p-3 text-neutral-content">Download Resume</button>
+        <button className="btn btn-primary btn-circle w-fit p-3 text-neutral-content">
+          <a href="/resume.pdf" download="Misato_Shimizu_Resume.pdf">Download Resume</a>
+        </button>
       </div>
     </header>
   );
